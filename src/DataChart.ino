cb24a3118f5b2f51635c499f15c447d8c2583d44
Zedespook <vasszili@gmail.com>
2020-10-16 15:07:42 +0200

    Removed OTA

--- src/DataChart.ino
+++ src/DataChart.ino
@@ -1,12 +1,8 @@
-#include <ESPmDNS.h>
-#include <WiFi.h>
-#include <WiFiUdp.h>
-
-#include <ArduinoOTA.h>
 #include <DS3231.h>
 #include <EmonLib.h>
 #include <SPIFFS.h>
 #include <WebServer.h>
+#include <WiFi.h>
 #include <Wire.h>
 
 // Ezzel könnyebb lesz majd állítani a maximum áramerősséget.
@@ -449,42 +445,6 @@ void MakeIndexPage()
 	html_file.close();
 }
 
-void OtaInitialization()
-{
-	ArduinoOTA
-		.onStart([]() {
-			String type;
-			if (ArduinoOTA.getCommand() == U_FLASH)
-				type = "sketch";
-			else // U_SPIFFS
-				type = "filesystem";
-
-			// NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-			Serial.println("Start updating " + type);
-		})
-		.onEnd([]() {
-			Serial.println("\nEnd");
-		})
-		.onProgress([](unsigned int progress, unsigned int total) {
-			Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-		})
-		.onError([](ota_error_t error) {
-			Serial.printf("Error[%u]: ", error);
-			if (error == OTA_AUTH_ERROR)
-				Serial.println("Auth Failed");
-			else if (error == OTA_BEGIN_ERROR)
-				Serial.println("Begin Failed");
-			else if (error == OTA_CONNECT_ERROR)
-				Serial.println("Connect Failed");
-			else if (error == OTA_RECEIVE_ERROR)
-				Serial.println("Receive Failed");
-			else if (error == OTA_END_ERROR)
-				Serial.println("End Failed");
-		});
-
-	ArduinoOTA.begin();
-}
-
 // Init
 void setup()
 {
@@ -497,7 +457,6 @@ void setup()
 
 	NetworkInitialization();
 	ClockInitialization();
-	OtaInitialization();
 
 	sensors[0].emon.current(32, 10);
 	sensors[1].emon.current(33, 10);
@@ -508,6 +467,5 @@ void setup()
 void loop()
 {
 	Server.handleClient();
-	ArduinoOTA.handle();
 	GetCurrentData();
 }
